e1280ff6b7b3548f9ee834f2ab2b4ae898f271d7
yousef-alaa-mostafa <yousefalaa67@gmail.com>
2022-09-03 18:11:56 +0200

    update

--- client/src/Components/Registration/Login/Login.jsx
+++ client/src/Components/Registration/Login/Login.jsx
@@ -1,40 +1,74 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
+
 // used images
 import instagram from "./images/instagram.png";
 
 //used style
 import LoginStyle from "./Login.module.css";
 
+//used components
+import LeftPart from "./LeftPart";
+
+// fontawesome imports
+import { faFacebookSquare } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+// used images
+import AppStore from "../images/appStore.png";
+import GooglePlay from "../images/googlePlay.png";
+
 function Login() {
   return (
-    <div className={LoginStyle.main}>
-      <img src={instagram}></img>
+    <div>
+      <div className={LoginStyle.c1}>
+        <div className={LoginStyle.main}>
+          <LeftPart />
+          <div>
+            <div className={LoginStyle.rightPart}>
+              <img src={instagram} style={{ marginTop: "30px" }}></img>
+              <form className={LoginStyle.form}>
+                <div className={LoginStyle.input_Btn_Container}>
+                  <input
+                    type="text"
+                    placeholder="Phone number, username, or email"
+                  ></input>
+                  <input type="password" placeholder="Password"></input>
+                  <button>Log In</button>
+                  <div className={LoginStyle.spanContainer}>
+                    <div className={LoginStyle.leftSpan}></div>
+                    <div className={LoginStyle.middleSpan}>OR</div>
+                    <div className={LoginStyle.rightSpan}></div>
+                  </div>
+                </div>
+              </form>
+              <button className={LoginStyle.Login_With_Facebook}>
+                <span>
+                  <FontAwesomeIcon icon={faFacebookSquare} />
+                </span>
+                Log in with Facebook
+              </button>
+              <div className={LoginStyle.ForgotPassword}>
+                <a href="">Forgot password?</a>
+              </div>
+            </div>
+            <div className={LoginStyle.rightPart2}>
+              <p>
+                Don't have an account? <a href="">Sign up</a>
+              </p>
+            </div>
+            <div className={LoginStyle.rightPart3}>
+              <p>Get the app.</p>
+            </div>
 
-      <form className={LoginStyle.form}>
-        <div class="form-floating mb-3 form-group" id={LoginStyle.inputDiv}>
-          <input
-            type="email"
-            class="form-control form-control-sm"
-            id="floatingInput inputsm"
-            placeholder="name@example.com"
-          />
-          <label for="floatingInput inputsm">
-            Phone number, username, or email
-          </label>
+            <div className={LoginStyle.rightPart4}>
+              <img src={AppStore} height="40px"></img>
+              <img src={GooglePlay} height="40px"></img>
+            </div>
+          </div>
         </div>
-        <div class="form-floating">
-          <input
-            type="password"
-            class="form-control"
-            id="floatingPassword"
-            placeholder="Password"
-          />
-          <label for="floatingPassword">Password</label>
-        </div>
-
-        <button>Log In</button>
-      </form>
+      </div>
     </div>
   );
 }
