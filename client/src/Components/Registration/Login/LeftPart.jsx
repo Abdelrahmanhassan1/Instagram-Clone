d1982ed3d934d43cfa5767beebd6790554e69c50
yousef-alaa-mostafa <yousefalaa67@gmail.com>
2022-09-03 13:55:11 +0200

    update

--- client/src/Components/Registration/Login/LeftPart.jsx
+++ client/src/Components/Registration/Login/LeftPart.jsx
@@ -8,6 +8,8 @@ import img1 from "./images/img1.png";
 import img2 from "./images/img2.png";
 import img3 from "./images/img3.png";
 import img4 from "./images/img4.png";
+
+import AppStore from "../images/AppStore.png";
 import { Callbacks } from "jquery";
 
 function LeftPart() {
