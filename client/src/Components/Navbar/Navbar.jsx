83b2d9e1724e117aa6db7cbc64ca02a6b3a3adb9
AbdelrahmanHassan1 <abdelrahman.marei00@eng-st.cu.edu.eg>
2022-09-06 18:21:08 +0200

    adding NAVbar

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import CSSModule from "./navbar.module.css";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className={CSSModule.navbar}>
+      <div className={CSSModule.navbar__logo}>
+        <img
+          src="https://www.freepnglogos.com/uploads/logo-website-png/logo-website-website-logo-design-website-logo-design-0.png"
+          alt="logo"
+        />
+      </div>
+      <div className={CSSModule.search_box}></div>
+      <div className={CSSModule.navbar_links}>
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
