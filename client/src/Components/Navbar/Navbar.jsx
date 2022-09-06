566e2b8c5d5df57409f37cf05840a875dda0675e
AbdelrahmanHassan1 <abdelrahman.marei00@eng-st.cu.edu.eg>
2022-09-06 21:28:16 +0200

    adding NAVbar

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -1,20 +1,36 @@
 import React from "react";
+import InstagramIMG from "./assets/instagram.png";
+
 import CSSModule from "./navbar.module.css";
+import { FiHeart, FiPlusSquare, FiSend } from "react-icons/fi";
+import { GrHomeRounded } from "react-icons/gr";
+import { AiOutlineCompass } from "react-icons/ai";
 
 function Navbar() {
   return (
     <div className={CSSModule.navbar}>
-      <div className={CSSModule.navbar__logo}>
-        <img
-          src="https://www.freepnglogos.com/uploads/logo-website-png/logo-website-website-logo-design-website-logo-design-0.png"
-          alt="logo"
-        />
+      <div className={CSSModule.navbar_logo}>
+        <img src={InstagramIMG} alt="instgram icon" />
       </div>
-      <div className={CSSModule.search_box}></div>
-      <div className={CSSModule.navbar_links}>
-        <a href="#">Home</a>
-        <a href="#">About</a>
-        <a href="#">Contact</a>
+      <div className={CSSModule.search_box}>
+        <input type="text" placeholder="Search" />
+      </div>
+      <div className={CSSModule.navbar_icons}>
+        <button>
+          <GrHomeRounded />
+        </button>
+        <button>
+          <FiSend />
+        </button>
+        <button>
+          <FiPlusSquare />
+        </button>
+        <button>
+          <AiOutlineCompass />
+        </button>
+        <button>
+          <FiHeart />
+        </button>
       </div>
     </div>
   );
