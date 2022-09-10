98907c266967c13dce9f10281dd0aedfafe8431a
abdelrahmanhassan1 <abdelrahman.marei00@eng-st.cu.edu.eg>
2022-09-10 23:35:05 +0200

    adding profile dat section

--- client/src/Components/Profile/Profile.jsx
+++ client/src/Components/Profile/Profile.jsx
@@ -15,7 +15,7 @@ export default function Profile() {
           </div>
           <div className={CSSModule.profile_data}>
             <div className={CSSModule.profile_data_header}>
-              <h1 className={CSSModule.name}>John Doe</h1>
+              <h1 className={CSSModule.name}>abdelrahmanH</h1>
               <button style={{ border: "1px solid black" }}>
                 Edit profile
               </button>
@@ -23,12 +23,31 @@ export default function Profile() {
                 <img src={Settings} alt="Settings"></img>
               </button>
             </div>
-            <div className={CSSModule.profile_data_follow}></div>
-            <p className={CSSModule.title}>Web Developer</p>
-            <p className={CSSModule.description}>sadddddddddddd</p>
-            <div className={CSSModule.btn}>
-              <a href="#">Hire me</a>
+            <div className={CSSModule.profile_data_follow}>
+              <div className={CSSModule.profile_data_follow_info}>
+                <h2>0</h2>
+                <h3>Posts</h3>
+              </div>
+              <div className={CSSModule.profile_data_follow_info}>
+                <h2>0</h2>
+                <h3>
+                  <a href="http://localhost:3000/profile">Followers</a>
+                </h3>
+              </div>
+              <div className={CSSModule.profile_data_follow_info}>
+                <h2>0</h2>
+                <h3>
+                  <a href="http://localhost:3000/profile">Following</a>
+                </h3>
+              </div>
             </div>
+            <p className={CSSModule.fullName}>Abdelrahman Hassan</p>
+            <p className={CSSModule.description}>
+              Muslim <br />
+              Developer <br />
+              Designer <br />
+              Photographer <br />
+            </p>
           </div>
         </div>
         <hr></hr>
