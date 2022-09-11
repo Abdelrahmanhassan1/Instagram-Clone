00396c788eda7406c38a7e0a651df29f7a436e7b
abdelrahmanhassan1 <abdelrahman.marei00@eng-st.cu.edu.eg>
2022-09-11 12:01:17 +0200

    modigying signup

--- client/src/Components/Registration/SignUp/FormInfo.jsx
+++ client/src/Components/Registration/SignUp/FormInfo.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import CSSModule from "./SignUp.module.css";
 import InstagramIMG from "./images/instagram.png";
 import { FaFacebookSquare } from "react-icons/fa";
-import FormInputs from "./FormInputs";
+// import FormInputs from "./FormInputs";
 import FormPolicy from "./FormPolicy";
 
 export default function FormInfo() {
@@ -25,7 +25,33 @@ export default function FormInfo() {
           <div className={CSSModule.or_text}>OR</div>
         </div>
 
-        <FormInputs />
+        <div className={CSSModule.input_Btn_Container}>
+          <div className={CSSModule.input}>
+            <input id="email" type="text" placeholder=" "></input>
+            <label htmlFor="email" className={CSSModule.floatingLabel}>
+              Phone number, username, or email
+            </label>
+          </div>
+          <div className={CSSModule.input}>
+            <input id="email" type="text" placeholder=" "></input>
+            <label htmlFor="email" className={CSSModule.floatingLabel}>
+              Full Name
+            </label>
+          </div>
+          <div className={CSSModule.input}>
+            <input id="email" type="text" placeholder=" "></input>
+            <label htmlFor="email" className={CSSModule.floatingLabel}>
+              Username
+            </label>
+          </div>
+          <div className={CSSModule.input}>
+            <input id="password" type="password" placeholder=" "></input>
+            <label htmlFor="password" className={CSSModule.floatingLabel}>
+              Password
+            </label>
+          </div>
+        </div>
+        {/* <FormInputs /> */}
         <FormPolicy />
 
         <button className={CSSModule.facebookButton}>Sign up</button>
